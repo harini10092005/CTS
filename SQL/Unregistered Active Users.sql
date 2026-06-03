e146fff7a1b0ea2839ad8db7fa02fca7e273d8b3
harini10092005 <harini809802@gmail.com>
2026-06-03 23:26:35 +0530

    Add files via upload

---
+++ SQL/Unregistered Active Users.sql
@@ -0,0 +1,7 @@
+SELECT *
+FROM Users u
+WHERE u.registration_date >= CURDATE() - INTERVAL 30 DAY
+AND u.user_id NOT IN (
+    SELECT DISTINCT user_id
+    FROM Registrations
+);
---
+++ SQL/registration_trends.sql
@@ -0,0 +1,6 @@
+SELECT DATE_FORMAT(registration_date, '%Y-%m') AS month,
+COUNT(registration_id) AS total_registrations
+FROM Registrations
+WHERE registration_date >= CURDATE() - INTERVAL 12 MONTH
+GROUP BY month
+ORDER BY month;
